6bf1387ed58600e6f1a5bc55e83094570f1db9f8
AstraKlaus <endrew-k@yandex.ru>
2022-01-24 16:24:12 +0400

    Initial Commit

---
+++ GuitarFromZero/GuitarFromZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarFromZeroApp.swift
+//  GuitarFromZero
+//
+//  Created by Андрей Касьянов on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuitarFromZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
